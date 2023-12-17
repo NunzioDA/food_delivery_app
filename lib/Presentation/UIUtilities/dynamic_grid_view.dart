f4289ad70980fb297c524c2fdf520fbf2fb6bc8e
NunzioDA <nunziodamore@gmail.com>
2023-12-17 18:52:00 +0100

    Aggiunte funzioni distinte per fit loose e tight

--- lib/Presentation/UIUtilities/dynamic_grid_view.dart
+++ lib/Presentation/UIUtilities/dynamic_grid_view.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+/// Permette di decidere il fit degli oggetti nella griglia.
+/// [tight] indica di utilizzare [DynamicGridView.targetItemWidth]
+/// come dimensione minima di ogni oggetto, creando un limite più forte
+/// per le dimensioni degli oggetti.
+/// [loose] crea un limite più morbido, cercando di dare agli oggetti una
+/// dimensione simile a quella indicata.
+enum DynamicGridFit
+{
+  tight, 
+  loose
+}
 
 class DynamicGridView extends StatelessWidget{
   final EdgeInsets padding;
-  final double minItemSize;
+  final double targetItemWidth;
   final double spacing;
   final double runSpacing;
   final double? aspectRatio;
   final List<Widget> children;
+  final DynamicGridFit fit;
+ 
 
   const DynamicGridView({
     super.key,
@@ -14,7 +27,8 @@ class DynamicGridView extends StatelessWidget{
     this.spacing = 0,
     this.runSpacing = 0,
     this.aspectRatio,
-    required this.minItemSize,
+    this.fit = DynamicGridFit.tight,
+    required this.targetItemWidth,
     required this.children
   });
 
@@ -24,10 +38,18 @@ class DynamicGridView extends StatelessWidget{
       padding: padding,
       child: LayoutBuilder(
         builder: (context, constraints) {
-                        
+                          
           double availableSpace = (constraints.maxWidth);
                         
-          int maxItemPerRow = (availableSpace ~/ minItemSize);
+          int maxItemPerRow;
+
+          if(fit == DynamicGridFit.loose)
+          {
+            maxItemPerRow = (availableSpace / targetItemWidth).round();
+          }
+          else{
+            maxItemPerRow = (availableSpace ~/ targetItemWidth);
+          }
                                     
           if(maxItemPerRow ==0)
           {
@@ -38,6 +60,11 @@ class DynamicGridView extends StatelessWidget{
           double perWidgetSpace = availableSpace / maxItemPerRow;
           double itemWidth = perWidgetSpace;
           
+          print(targetItemWidth);
+          print(availableSpace);
+          print(targetItemWidth);
+          print(maxItemPerRow);
+
           return Wrap(
             direction: Axis.horizontal,
             runSpacing: runSpacing,
