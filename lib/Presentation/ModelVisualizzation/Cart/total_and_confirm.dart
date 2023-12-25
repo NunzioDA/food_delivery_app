67ae0d8d1a0f4d598b4502ea50b368d03f47b41c
NunzioDA <nunziodamore@gmail.com>
2023-12-25 19:31:49 +0100

    Aggiunto hitbehavior translucent, per avviare la gesture toccando in qualsiasi punto della parte superiore del pannello

--- lib/Presentation/ModelVisualizzation/Cart/total_and_confirm.dart
+++ lib/Presentation/ModelVisualizzation/Cart/total_and_confirm.dart
@@ -179,6 +179,7 @@ class TotalAndConfirmState extends State<TotalAndConfirm>
                   children: [
                     if(!UIUtilities.isHorizontal(context))
                     Listener(
+                      behavior: HitTestBehavior.translucent,
                       onPointerDown: (event){
                         wasOpened = isOpened();
                       },
